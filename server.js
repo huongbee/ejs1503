c2623cf9b21f615f50bb1fe8b380480af49db1f0
Huong <huongnguyenak96@gmail.com>
2019-03-29 19:10:22 +0700

    index.ejs

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const {arraySinger, prefixImg, prefixLink} = require('./models/Singer')
 const app = express();
 
 app.set('view engine','ejs');
 
 
-
+app.get('/',(req,res)=>{
+    res.render('index',{arraySinger, prefixImg, prefixLink})
+})
 
 app.listen(3000)
