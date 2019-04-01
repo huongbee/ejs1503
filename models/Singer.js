2fdfac8af09a687e16b09f800ba138886854451b
Huong <huongnguyenak96@gmail.com>
2019-04-01 18:10:15 +0700

    ejs

--- models/Singer.js
+++ models/Singer.js
@@ -12,7 +12,6 @@ const prefixLink = 'https://zingmp3.vn/nghe-si/'
 let arraySinger = [
     new Singer(1,'Mr Siro','e/1/1/2/e1120261421cfec7513423222b0ca94c.jpg','Mr-Siro'),
     new Singer(2,'K-ICM','e/e/e/e/eeee188a74736ed2423d4231b6b41424.jpg','K-ICM'),
-    new Singer(3,'Hương Giang Idol','d/4/e/f/d4ef2628425fb5a456e23cce31b50a2e.jpg','Huong-Giang-Idol'),
-    new Singer(3,'Hơng Giang Idol','d/4/e/f/d4ef2628425fb5a456e23cce31b50a2e.jpg','Huong-Giang-Idol')
+    new Singer(3,'Hương Giang Idol','d/4/e/f/d4ef2628425fb5a456e23cce31b50a2e.jpg','Huong-Giang-Idol')
 ]
 module.exports = {Singer,prefixImg, prefixLink, arraySinger}
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const {arraySinger, prefixImg, prefixLink} = require('./models/Singer')
+const {arraySinger, prefixImg, prefixLink, Singer} = require('./models/Singer')
+const bodyParser = require('body-parser')
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}))
 app.set('view engine','ejs');
-
 
 app.get('/',(req,res)=>{
     res.render('index',{arraySinger, prefixImg, prefixLink})
@@ -13,9 +14,15 @@ app.get('/add-singer',(req,res)=>{
     res.render('add')
 })
 app.post('/add-singer',(req,res)=>{
-    // 
-    // res.render('add')
+    const { name, avatar, link } = req.body
+    const singer = new Singer(
+        Math.round(Math.random()*1000),
+        name,
+        avatar,
+        link
+    )
+    arraySinger.push(singer)
+    return res.redirect('/')
 })
-
 
 app.listen(3000)
